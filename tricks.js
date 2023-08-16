467b65f13727241973496d6eb660f9a148b18c58
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-08-16 16:42:12 +0530

    gg

---
+++ tricks.js
@@ -0,0 +1,10 @@
+// console.log(5+ +1)
+// console.log("A"+ +"B")
+// console.log(5+ +"A")
+// console.log(5+ -1)
+// console.log(0.1 + 0.2)
+
+console.log("A" + 1 + "B")
+console.log("A" - 1 + "B")
+console.log("A" - "C" + "B")
+console.log("A" - 5 + "-1")
