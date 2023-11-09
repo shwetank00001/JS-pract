6462cbfc39fd10059f54d349ac34afd14234d242
shwetank00001 <mshwetank@gmail.com>
2023-11-09 10:57:32 +0530

    gg

--- js basics/promises.js
+++ js basics/promises.js
@@ -1,22 +1,24 @@
-function getData(){
-    return new Promise( (resolve,reject ) => {
-        const value = true
-        if(value){
-            const data = {"id" : 1, "name" : "shwetank"}
+function showData(){
+    return new Promise( (resolve, reject) => {
+        const a = true;
+        if(a){
+            const data = {"id": 1, "name":"Shwetank"}
             resolve(data)
         }
-        else
-        reject(new Error("New error while doing this"))
+
+        else {
+            reject(new Error("New error"))
+        }
     })
 }
 
-async function showData(){
+async function getData(){
     try {
-        const newData = await getData()
-        console.log(newData)
+        const value = await showData()
+        console.log(value)
     } catch (error) {
         console.log(error)
     }
 }
 
-showData()
+getData()
