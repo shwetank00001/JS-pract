c8b16098f2da515143f49d5a88a45f51a5f03c52
shwetank00001 <mshwetank@gmail.com>
2023-11-18 11:48:54 +0530

    gg

--- js basics/filter.js
+++ js basics/filter.js
@@ -1,3 +1,6 @@
 const array = [ 1, 1, 2, 3, 5, 5, 1 ]
 const uniqueArray = [...new Set(array)];
 console.log(uniqueArray); // Result: [1, 2, 3, 5]
+
+const a = ["av", "Abc", "sr", "asf","sfb", "vxvb"]
+const ele = a.filter(startsWith("a"));
--- js basics/splicing.js
+++ js basics/splicing.js
@@ -1,5 +1,5 @@
 var a = [1,2,3,4,5,6,7,8,89,10]
 
-b= a.splice(3,9)
+b= a.splice(3,5)
 
 console.log(b)
