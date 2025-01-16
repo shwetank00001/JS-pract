83eb73bc58be9442637f2a2c55e822780a3da6f5
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-16 14:02:03 +0530

    Block scoped and how shadowing works in JS

---
+++ 09BlockScoped.js
@@ -0,0 +1,45 @@
+{
+    var a = 10
+    let b = 20
+    const c = 30
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+
+let x = 10;
+{
+    let x = 50
+    console.log(x)
+}
+console.log(x)
+
+
+let u = 10
+{
+    let v = 20
+    var aw = 30
+    const y = 40
+    console.log(v)
+    console.log(aw)
+    console.log(y)
+}
+console.log(u)
+
+
+var a_ = 50
+{
+    let a_ = 500
+}
+
+
+const blo = 50
+{
+    const blo = 100
+    console.log(blo)
+    {
+        const blo = 99
+
+    }
+}
