44b59338bf739340fdb38904d6dc68b303daa392
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-17 14:30:31 +0530

    gg

--- 10Closures.js
+++ 10Closures.js
@@ -1,11 +1,23 @@
-// Closure : A function 'y()' bind together with it's lexical scope ['y()'s local memory + x()'s lexical env']
+// Closure : A function 'y()' bind together with it's lexical scope ['y()'s local memory + x()'s lexical env'] 
+// You borrow your mom's purse (outer function). Even when she leaves the room, you can still use the purse (inner function) because you have it with you. The purse contains her belongings (variables), and you can access them.
+
 
 function x(){
     var a = 20
-    var b = 200
-    y()
     function y(){
         console.log(a)
     }
+    return y;  // this returns the value of x() = func y()
 }
-x()
+var z = x()
+console.log("x value => ", z)
+
+// function  abc(){
+//     var _ = 50
+//     function xyz(){
+//         console.log(_)
+//     }
+//     console.log(xyz)
+// }
+
+// abc()
