7706efd72cc13d15d2149cbf996129badbd1c47f
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-20 23:21:05 +0530

    gg

---
+++ 10ClosuresTest.js
@@ -0,0 +1,29 @@
+function x(){
+    var i = 10;
+    function y(){
+        console.log(i)
+    }
+    y()
+}
+x()
+
+var z  = x()
+
+
+
+// var a= 70
+
+// function x(){
+//     var a = 50
+//     let b = 10
+//     function y(){
+//         console.log(a,b)
+//     }
+//     y()
+// }
+
+// function z(){
+//     console.log(a)
+// }   
+// x()
+// z()
