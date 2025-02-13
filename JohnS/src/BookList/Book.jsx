bbc7512736001a4cd808b155a3f3887e866181ac
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-02-13 18:25:47 +0000

    Will work on this tonight

--- JohnS/src/BookList/Book.jsx
+++ JohnS/src/BookList/Book.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 // import Image from './Image'
-import Author from './Author'
+import Author from "./Author";
 
 function Book(props) {
   /*
@@ -15,19 +15,18 @@ function Book(props) {
   //   const image = 'https://m.media-amazon.com/images/I/51eQekkEKoL._AC_UY327_FMwebp_QL65_.jpg'
   // const title = "React.js For Beginners"
 
-  let {image, title, author} = props
+  let { image, title, author } = props;
   return (
-
-    <article className='book'>
-        <img src= {image} alt=''/>
-        <p>{title}</p>
-        <Author author = {author}/>
-        {props.children}
+    <article className="book">
+      <img src={image} alt="" />
+      <p>{title}</p>
+      <Author author={author} />
+      {props.children}
     </article>
-  )
+  );
 }
 // const Image = () => <img src='https://m.media-amazon.com/images/I/51eQekkEKoL._AC_UY327_FMwebp_QL65_.jpg' alt=''/>
 
 // const Title = () => <p>React.js For Beginners</p>
 
-export default Book
+export default Book;
--- JohnS/src/BookList/Booklist.jsx
+++ JohnS/src/BookList/Booklist.jsx
@@ -1,45 +1,32 @@
-import React from 'react'
-import Book from './Book'
-import './booklist.css'
+import React from "react";
+import Book from "./Book";
+import "./booklist.css";
 
 function Booklist() {
-
-  const project2 = {
-    image : "https://m.media-amazon.com/images/I/51RXyeWjE1L.AC_SX250.jpg",
-    title : "I will be selected",
-    author : "Only Shwetank"
-  }
-  const project3 = {
-    image : "https://images-eu.ssl-images-amazon.com/images/I/81+ceFx9BcL._AC_UL600_SR600,400_.jpg",
-    title : "Book 3",
-    author : "Author 3"
-  }
+  const bookArray = [
+    {
+      image: "https://m.media-amazon.com/images/I/51RXyeWjE1L.AC_SX250.jpg",
+      title: "I will be selected",
+      author: "Only Shwetank",
+    },
+    {
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/81+ceFx9BcL._AC_UL600_SR600,400_.jpg",
+      title: "Book 3",
+      author: "Author 3",
+    },
+  ];
+  const names = ["L1", "L2", "L3"];
+  const newNames = names.map(function (item) {
+    // const id = new Date().getTime()
+    return <div key={item.id}>{item}</div>;
+  });
   return (
-    <section className='booklist'>
-        <Book 
-        image = 'https://m.media-amazon.com/images/I/51eQekkEKoL._AC_UY327_FMwebp_QL65_.jpg'
-        title = "Selected at EY!!" 
-        author = "Shwetank Mishra"
-        >
-                    <p>I will definitely become SDE1 by the end of this month!</p>
-        </Book>
-        <Book 
-               image = {project2.image}
-               title = {project2.title} 
-               author = {project2.author}
-        >
-          <p>Testing CHildren 2</p>
-        </Book>
-        {/* <Book 
-          image = {project3.image}
-          title = {project3.title}
-          author = {project3.author}
-          // author = {project2.author}
-          /> */}
+    <section className="booklist">
+      {/* <Book /> */}
+      {newNames}
     </section>
-  )
+  );
 }
 
-
-
-export default Booklist
+export default Booklist;
