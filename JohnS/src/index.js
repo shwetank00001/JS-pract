499c1d6ffe182a0d4f5ec3ea8cf9507f055b0cb8
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-02-16 01:39:50 +0530

    Modal pop up using setTimeout lol

--- JohnS/src/index.js
+++ JohnS/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Reducer from './useReducer/Reducer'
+import RedBasics from './useReducer/RedBasics'
 import './index.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Reducer />
+  <RedBasics />
   // <React.StrictMode>
   // </React.StrictMode>
 );
--- JohnS/src/useReducer/Modal.jsx
+++ JohnS/src/useReducer/Modal.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = (props) => {
+  const [value, setValue] = React.useState(props.showModal1)
+
+  setTimeout(()=> {
+    setValue(false)
+  }, 2000)
   return (
-    <div>Modal</div>
+    <div>
+      {value && <h2>Value inserted</h2>}
+    </div>
   )
 }
 
