d10cfeb671a1f9e803d273f1ef8ab8c817f874b5
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-02-21 20:19:48 +0000

    Today

---
+++ JohnS/src/Fetch/Test.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Test = () => {
+  return (
+    <div>
+        <h1>Devende11r</h1>
+        <Test1/>
+    </div>
+  )
+}
+export const Test1 = () => {
+  return (
+    <div>
+        <h1>test 1</h1>
+        
+    </div>
+  )
+}
+
+export default Test
--- JohnS/src/index.js
+++ JohnS/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import {ReactMe} from './ReactMemoMethod/ReactMe';
-import Fetch from './Fetch/Fetch'
+import Fetch from './Fetch/Test'
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
