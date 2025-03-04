8dfd795ac52af16f6a837a55349aff6430536bcb
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-03-04 16:52:06 +0000

    working

--- JohnS/src/useContext/ContextAPI.jsx
+++ JohnS/src/useContext/ContextAPI.jsx
@@ -1,57 +1,49 @@
-import React from 'react'
-import { dataMain } from './data'
-import { useContext } from 'react'
+import React from "react";
+import { dataMain } from "./data";
+import { useContext } from "react";
 
-const PeopleContext = React.createContext()
+const PeopleContext = React.createContext();
+//this provides us a Provider (distributor) and a Consumer (the component which wants the data) -> this is replaced by useContext
 
 const ContextAPI = () => {
-
-  const [people, setPeople] = React.useState(dataMain)
-  const removeData = function(idParam){
-    return (
-      setPeople(function(item){
-        return item.filter(function(item){
-          return item.id !== idParam
-        })
-      })
-    )
-  }
+  const [people, setPeople] = React.useState(dataMain);
+  const removeData = function (idParam) {
+    return setPeople(function (item) {
+      return item.filter(function (item) {
+        return item.id !== idParam;
+      });
+    });
+  };
 
   return (
-    <PeopleContext.Provider value ={{people, removeData}}>
-      <List 
-      />
+    <PeopleContext.Provider value={{ people, removeData }}>
+      <List />
     </PeopleContext.Provider>
-  )
-}
+  );
+};
 
-let List = function(props){
+let List = function (props) {
   return (
     <section>
-      <SingleData  />
+      <SingleData />
     </section>
-  )
-}
+  );
+};
 
-let SingleData = function(props){
-  let {people, removeData} = useContext(PeopleContext)
+let SingleData = function (props) {
+  let { people, removeData } = useContext(PeopleContext);
   return (
     <div>
-      {
-        people.map(function(item){
-          return (
-            <div key={item.id}>
-              
-              <h3>{item.name}</h3>
-              <button onClick={() => removeData(item.id)}>Del</button>
-              
-            </div>
-          )
-        })
-      }
-
+      {people.map(function (item) {
+        return (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <button onClick={() => removeData(item.id)}>Del</button>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ContextAPI
+export default ContextAPI;
---
+++ JohnS/src/useContext/Test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { dataMain } from "./data";
+
+function Test() {
+  return <div>Test</div>;
+}
+
+export default Test;
