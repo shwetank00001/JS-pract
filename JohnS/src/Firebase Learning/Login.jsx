53369a64cc88d91eca5911f3af1a7b8a2244b95d
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-03-05 14:08:50 +0000

    Sending signup data from FE to firebase

--- JohnS/src/Firebase Learning/Login.jsx
+++ JohnS/src/Firebase Learning/Login.jsx
@@ -1,51 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { auth } from "./Firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [userData, setUserdata] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [userData , setUserdata] = useState([])
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [textDisplay, setText] = useState();
 
-    let handleSub = async function(e){
-        e.preventDefault()
-        const newData = {
-            id: new Date().getTime(),
-            email : email,
-            password: password
-        }
-        setUserdata(function(item){
-            return [...item, newData]
-        })
+  let handleSub = async function (e) {
+    e.preventDefault();
+    const newData = {
+      id: new Date().getTime(),
+      email: email,
+      password: password,
+    };
+    setUserdata(function (item) {
+      return [...item, newData];
+    });
 
-        try {
-            const userCreds = await createUserWithEmailAndPassword(auth,email,password)
-            console.log("User Signed up with", userCreds)
-        } catch (error) {
-            console.log(error)
-        }
+    try {
+      const userCreds = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("User Signed up with", userCreds);
+      setText("User created in firebase DB.");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const ele = userData.map((item) => {
-        return (
-            <div key={item.id}>
-                <h3 >{item.email}</h3>
-                <p>{item.password}</p>
-            </div>
-        )
-    })
+  // const ele = userData.map((item) => {
+  //     return (
+  //         <div key={item.id}>
+  //             <h3 >{item.email}</h3>
+  //             <p>{item.password}</p>
+  //         </div>
+  //     )
+  // })
 
   return (
-    <div className='container mx-auto bg-slate-600 p-3 ' >
-        <form className='flex gap-3' onSubmit={handleSub}>
-            <input className='border-2 border-blue-500 p-2 rounded-md' type='text' name = "email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input className='border-2 border-blue-500 p-2 rounded-md' type='password' name = "password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button className='text-white' type='submit'>submit</button>
-        </form>
-        {ele}
+    <div className="container mx-auto bg-slate-600 p-3 ">
+      <form className="flex gap-3" onSubmit={handleSub}>
+        <input
+          className="border-2 border-blue-500 p-2 rounded-md"
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="border-2 border-blue-500 p-2 rounded-md"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="text-white" type="submit">
+          submit
+        </button>
+      </form>
+      {/* {ele} */}
+      {textDisplay}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
