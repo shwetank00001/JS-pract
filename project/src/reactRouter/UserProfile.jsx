6f593964fadff78e0818206dbb69e6284f218f84
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-03-25 04:36:47 +0530

    LOCKED IN AGAIN

--- project/src/reactRouter/UserProfile.jsx
+++ project/src/reactRouter/UserProfile.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router'
 
 const UserProfile = () => {
+
+  // this params will take the :name property from the url and we can access this later
     const params = useParams();
     console.log(params)
 
