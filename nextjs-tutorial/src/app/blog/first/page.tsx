fed3fa07796915ccfdab558d5ccf41513ed974c4
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-03-31 14:46:58 +0530

    Gg back on grind from today

---
+++ nextjs-tutorial/src/app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function first(){
+    return (
+        <div>This is the first blog </div>
+    )
+}
--- project/first-nextjs/src/app/page.tsx
+++ project/first-nextjs/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home(){
-    return <div>Hello</div>
+    return <h1>hi</h1>;
 }
