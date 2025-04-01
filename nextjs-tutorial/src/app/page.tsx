535c518d81d61432c68e65a8d608239c9899f3b5
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-04-01 16:25:41 +0000

    Link

--- nextjs-tutorial/src/app/page.tsx
+++ nextjs-tutorial/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Home(){
-    return <div>Hello</div>
+    return (
+    <div>
+        Hello, this is the home page for our website.
+        <Link href="/profile">Go to profile page</Link>
+    </div>
+)
 }
