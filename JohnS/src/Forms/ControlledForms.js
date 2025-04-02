530915c26c749f24b001c4ece902d9763cfbe54b
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-04-02 16:38:58 +0000

    Will revise JS on with FCC

--- JohnS/src/Forms/ControlledForms.js
+++ JohnS/src/Forms/ControlledForms.js
@@ -1,40 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const ControlledForms = () => {
-    // const [data, setData ] = React.useState("")
-    // const [lName, setName ] = React.useState("")
-    const [formData, setForm] = React.useState({
-        fname: "",
-        lname:"",
-        areURich: false
-    })
+  // const [data, setData ] = React.useState("")
+  // const [lName, setName ] = React.useState("")
+  const [formData, setForm] = React.useState({
+    fname: "",
+    lname: "",
+    areURich: false,
+  });
 
-    function handleChange(e) {
-        const { name, value, type, checked } = e.target;
-    
-        setForm((prevForm) => ({
-          ...prevForm,
-          [name]: type === 'checkbox' ? checked : value
-        }));
-      }
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target;
 
-    function handleSubmit(e){
-        e.preventDefault()
-        console.log(formData)
-    }
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+  }
 
   return (
     <div>
-        <form  onSubmit={handleSubmit}>
-        <input type='text' name="fname" value={formData.fname} placeholder='first Name' onChange={handleChange} />
-        <input type='text' name="lname" value={formData.lname} placeholder='Last name' onChange={handleChange}/>
-        <label htmlFor='richh'>Are you rich</label>
-        <input type="checkbox" name="areURich" id="rich" checked={formData.areURich} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fname"
+          value={formData.fname}
+          placeholder="first Name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="lname"
+          value={formData.lname}
+          placeholder="Last name"
+          onChange={handleChange}
+        />
+        <label htmlFor="richh">Are you rich</label>
+        <input
+          type="checkbox"
+          name="areURich"
+          id="rich"
+          checked={formData.areURich}
+          onChange={handleChange}
+        />
         <button>Submit</button>
-        </form>
-
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ControlledForms
+export default ControlledForms;
