226616e7d64ee8ac3011898eea0f468a51cc4bc8
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-04-24 14:35:54 +0000

    gg

--- project/src/vanillaCss/Layout.jsx
+++ project/src/vanillaCss/Layout.jsx
@@ -1,7 +1,16 @@
+import "./style.css";
+
 function Layout() {
   return (
     <div>
-      <div></div>
+      <header>header</header>
+      <div className="middle">
+        <div className="main-content">main-content</div>
+        <div className="sidebar">sidebar</div>
+        <div className="twin">twin</div>
+        <div className="twin">twin</div>
+      </div>
+      <footer>footer</footer>
     </div>
   );
 }
