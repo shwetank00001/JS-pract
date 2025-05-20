05f191a7f353f6a906ad38e9c588639fb9ba0eaf
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-05-20 14:40:10 +0000

    states revision done

--- revise1/src/hooks/state/State.jsx
+++ revise1/src/hooks/state/State.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { useState } from "react";
 
 const State = () => {
-  return <div></div>;
+  const [peopleQueue, setPeopleQueue] = useState([]);
+  const [personName, setPersonName] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("personName", personName);
+    const keyedPerson = {
+      id: new Date().getTime(),
+      personName: personName,
+    };
+    setPeopleQueue(function (item) {
+      return [...item, keyedPerson];
+    });
+  }
+  console.log(peopleQueue);
+
+  const removePerson = (idParam) => {
+    const newPeopleList = peopleQueue.filter(function (item) {
+      return item.id !== idParam;
+    });
+    setPeopleQueue(newPeopleList);
+  };
+
+  const displayPeopleList = peopleQueue.map(function (item) {
+    return (
+      <div key={item.id}>
+        <p>{item.personName}</p>
+        <button
+          onClick={() => {
+            removePerson(item.id);
+          }}
+        >
+          Remove from Queue
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <p>Lets manage state using useState hook.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={personName}
+          type="text"
+          onChange={(e) => setPersonName(e.target.value)}
+        />
+        <button type="submit">Add in queue</button>
+      </form>
+      <section>
+        <h3>List of all the people in line:</h3>
+        {displayPeopleList}
+      </section>
+    </>
+  );
 };
 
 export default State;
