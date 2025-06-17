1c15c9f64c8fc72b4fd5aefaed31278e7bea68e0
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-06-17 05:41:07 +0530

    gg rip

---
+++ Namaste JS/Objects/objectarray.js
@@ -0,0 +1,13 @@
+const gymEx = [
+    {
+        chest : ["Upper", "Middle", "Lower"]
+    },
+    {
+        back : ["Lats", "Traps", "Middle"]
+    }
+]
+
+const list = gymEx.map(function(item){
+    return item.chest
+})
+console.log(list)
