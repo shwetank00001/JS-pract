383b473811c3cdfd0381b4f36ef2c8ccd2d2961a
Shwetank <51539873+shwetank00001@users.noreply.github.com>
2025-06-24 13:26:26 +0000

    made a notes app with add and delete

--- everyHookProject/src/Components/Notes.jsx
+++ everyHookProject/src/Components/Notes.jsx
@@ -3,13 +3,21 @@ import { useReducer, useState } from "react";
 export default function Notes(){
 
     function reducer(state, action){
-        if(type === "ADD"){
+        if(action.type === "ADD"){
             const newStateValue = [...state.peopleList, action.payload];
             return {
-                ...state,
-                peopleList: [newStateValue]
+                peopleList: newStateValue
             }
         }
+        else if(action.type === "DELETE"){
+            const newStateValue = state.peopleList.filter(function(item){
+                return item.id !== action.payload;
+            })
+            return {
+                peopleList: newStateValue
+            }
+        }
+
     }
 
     const defaultState = {
@@ -17,7 +25,7 @@ export default function Notes(){
 
     }
     
-    const [state, dispatch] = useReducer(defaultState, reducer);
+    const [state, dispatch] = useReducer(reducer, defaultState);
     const [inputPeople, setInputData] = useState([]);
 
     function handleSubmit(e){
@@ -26,23 +34,30 @@ export default function Notes(){
             id: new Date().getTime(),
             peopleName: inputPeople
         }
-        dispatch({type:"ADD", payload: newData})
+        dispatch({type:"ADD", payload: newData});
+        console.log(inputPeople)
+    }
+
+    const remove = (idParam) => {
+        dispatch({type:"DELETE", payload: idParam})
     }
     
     const ele = state.peopleList.map(function(item){
         return (
-            <>
-                <h3>{item.peopleName}</h3>
-            </>
+            <div key= {item.id}>
+                <p >{item.peopleName}</p>
+                <button onClick={() => {remove(item.id)}}>-</button>
+            </div>
         )
     })
     return (
         <>
             <h1>A notes app with useReducer</h1>
-            <form onClick={handleSubmit}>
-                <textarea value={state.peopleList} onChange={e => setInputData(e.target.value)} />
+            <form onSubmit={handleSubmit}>
+                <textarea value={inputPeople} onChange={e => setInputData(e.target.value)} />
                 <button type="submit">Add</button>
             </form>
+            {ele}
         </>
     )
 }
