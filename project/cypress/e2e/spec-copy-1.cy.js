42f034927695d2f4f4e2780533be2506aac474ae
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-07-08 03:14:41 +0530

    Learny cypress basics

--- project/cypress/e2e/spec-copy-1.cy.js
+++ project/cypress/e2e/spec-copy-1.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('[data-test="main-header"]').contains("This is the main page!")
   })
 })
--- project/src/router/Main.jsx
+++ project/src/router/Main.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 export default function Main() {
   return (
     <div>
-      <h1>This is main</h1>
+      <h1 data-test="main-header">This is the main page!</h1>
       <h3>Clicking this will take you to the users list page.</h3>
       <Link to="/people">People List (Only accessible if logged In!)</Link>
       <hr />
