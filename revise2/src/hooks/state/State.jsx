ffd6ef53dc391236eb15bd09e1d90010c9b22b43
Shwetank <mshwetank@gmail.com>
2025-11-05 09:45:05 +0530

    revision for state + ternary + Conditional done

--- revise2/src/hooks/state/State.jsx
+++ revise2/src/hooks/state/State.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const state = () => {
+
+  const [things, setThings] = useState(['Thing 1', 'Thing 2']);
+
+  const ele = things.map(function(item){
+    return (
+      <>
+        <h3>{item}</h3>
+      </>
+    )
+  })
+
+  function addThings(){
+    setThings(function(item){
+      return [...item, `Thing ${things.length + 1}`]
+    })
+  }
+
+  function showHideData(){
+    setShow(i => !i)
+  }
+  const [showData, setShow] = useState(true); // revising CONDITIONAL operator
   return (
-    <div>state</div>
+    <div>
+      <button onClick={showHideData}>Show THis ele data</button>
+  
+     { showData &&  
+     <div>
+      {ele}
+      </div>
+     }
+      <button onClick={addThings}>Add</button>
+
+
+      <p>The value of the this text changes when the state of 'showData' changes= {showData ? "Visible Data" : "Hidden Data"}</p>
+
+    </div>
   )
 }
 
--- revise2/src/main.jsx
+++ revise2/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './hooks/Context/Home'
 import {BrowserRouter} from 'react-router'
-import DebounceAPI from './hooks/debounce/DebounceAPI'
+import State from './hooks/state/State'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <DebounceAPI />  
+      <State />  
     </BrowserRouter>
   </StrictMode>,
 )
