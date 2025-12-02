ff5a3fad7812b201051bb2bec32c67b3f41957b3
Shwetank <mshwetank@gmail.com>
2025-12-02 10:13:58 +0530

    Fixed router rev

--- JohnS/src/RouterTest/SecretData.jsx
+++ JohnS/src/RouterTest/SecretData.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 function SecretData(){
+    const names = [
+        {
+            id: 1,
+            name: "Shwetank"
+        },
+        {
+            id: 2,
+            name: "Mishra"
+        },
+        {
+            id: 3,
+            name: "Welocalize"
+        },
+]
+
+const ele = names.map((item) => {
+    return (
+        <div key={item.id}>
+            <Link to={`/welcome/${item.name}`}>{item.name}</Link>
+        </div>
+    )
+})
     return (
         <>
             <p>Secret Data is this</p>
+            {ele}
         </>
     )
 }
--- dev.js
+++ dev.js
@@ -1,16 +1,1 @@
-// var a =200
-// function bc(){
-//     if(true){
-//         var a= 100;
-//         console.log(a)
-//     }
-//     console.log(a)
-// }
-// console.log(a)
-// bc();
-// console.log(a)
-
-const a = 10;
-a = 10000;
-
-console.log(a)
+console
