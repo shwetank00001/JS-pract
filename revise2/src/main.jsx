beb6931baf4c34448417d2ead8dd3e35902e03d7
Shwetank <mshwetank@gmail.com>
2025-12-03 10:24:27 +0530

    A promise always stores an objext

--- revise2/src/main.jsx
+++ revise2/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Hero from './searchbar/Hero'
+import Promise from './promise/PromiseTest'
 import {BrowserRouter} from 'react-router'
 import State from './hooks/state/State'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Hero />  
+      <Promise />  
     </BrowserRouter>
   </StrictMode>,
 )
---
+++ revise2/src/promise/PromiseTest.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const PromiseTest = () => {
+
+    const cart = ["laptop", "shoes", "bag"];
+
+    const promise1 = () => {
+        return new Promise((resolve, reject) => {
+            if(cart){
+                const area = {
+                    id: 1,
+                    name: "Shwetank",
+                    location : "Noida"
+                };
+                resolve("areaadljlakdj", area)
+            }
+            else reject("Error fetching cart");
+        })
+    }
+
+    promise1().then((data) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log("Error:", err)
+    })
+
+    
+  return (
+    <div>
+        <h3>Test</h3>
+    </div>
+  )
+}
+
+export default PromiseTest
